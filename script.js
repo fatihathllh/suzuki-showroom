96fcff2a7a6a30bafa97cce430878a55fda344c6
fatihathllh <152984653+fatihathllh@users.noreply.github.com>
2026-09-04 04:21:21 +0000

    SUZUKI-SHOWROOM

--- script.js
+++ script.js
@@ -1,31 +1,48 @@
 function openModal(carName) {
 
-    const modal = document.getElementById("productModal");
-    const title = document.getElementById("modalTitle");
-    const text = document.getElementById("modalText");
+```
+const modal = document.getElementById("productModal");
+const title = document.getElementById("modalTitle");
+const text = document.getElementById("modalText");
+const whatsappButton = document.getElementById("whatsappButton");
 
-    title.innerText = carName;
+title.innerText = carName;
 
-    text.innerText =
-        "Hubungi Muhammad Fatih Athallah untuk mendapatkan informasi harga, promo, simulasi kredit, dan ketersediaan unit " + carName + ".";
+text.innerText =
+    "Hubungi Muhammad Fatih Athallah untuk mendapatkan informasi harga, promo, simulasi kredit, dan ketersediaan unit " + carName + ".";
 
-    modal.style.display = "block";
+const nomorWhatsApp = "6289516129537";
+
+const pesan =
+    "Halo, saya ingin menanyakan informasi harga dan promo " + carName;
+
+const linkWhatsApp =
+    "https://wa.me/" + nomorWhatsApp + "?text=" +
+    encodeURIComponent(pesan);
+
+whatsappButton.href = linkWhatsApp;
+
+modal.style.display = "block";
+```
+
 }
-
 
 function closeModal() {
 
-    document.getElementById("productModal").style.display = "none";
+```
+document.getElementById("productModal").style.display = "none";
+```
 
 }
 
-
 window.onclick = function(event) {
 
-    const modal = document.getElementById("productModal");
+```
+const modal = document.getElementById("productModal");
 
-    if (event.target === modal) {
-        modal.style.display = "none";
-    }
+if (event.target === modal) {
+    modal.style.display = "none";
+}
+```
 
 };
